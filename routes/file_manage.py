07135c022cb8c61d9ac4a9af76ae1220f8eac5aa
Nikhil Tanwar <2002nikhiltanwar@gmail.com>
2024-02-12 10:42:23 +0530

    Only allow OPS user to upload files
    Added restrictions on upload file endpoint

--- routes/file_manage.py
+++ routes/file_manage.py
@@ -1,15 +1,20 @@
 from db import models, db
-from fastapi import FastAPI, UploadFile, HTTPException, APIRouter
+from fastapi import FastAPI, UploadFile, HTTPException, APIRouter, Depends
+from .user_manage import get_current_user
+from typing import Annotated
+from schema.schema import User, Role
 
 router = APIRouter()
 
 @router.post("/uploadfile/", status_code=201)
-async def create_upload_file(file: UploadFile):
+async def create_upload_file(file: UploadFile, current_user: Annotated[User, Depends(get_current_user)]):
     mime_types = ["application/vnd.openxmlformats-officedocument.wordprocessingml.document",
                   "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
                   "application/vnd.openxmlformats-officedocument.presentationml.presentation"]
+    if (current_user.role == Role.OPS.name):
+        raise HTTPException(401, detail="only OPS user can upload files")
     if (file.content_type not in mime_types):
-        raise HTTPException(400, detail="file type is not allowed")
+        raise HTTPException(401, detail="file type is not allowed")
     is_file_added = await add_file_to_db(file)
     if (is_file_added):
         return {"message": "file added successfully"}
